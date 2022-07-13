322a3685bb67b025b541f862772249d66f98080f
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-13 00:47:08 -0300

    test: ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,12 +3,12 @@ import { MongoClient } from "mongodb";
 
 export const mongoHelper = {
   client: null as MongoClient,
-  async connect(url: string): Promise<void> { // eslint-disable-line
+  async connect(): Promise<void> {
     this.client = await MongoClient.connect(process.env.MONGO_URL);
   },
 
   async disconnect(): Promise<void> {
-    await this.client.close;
+    await this.client.close();
   },
 
   getCollection(name: string): Collection {
