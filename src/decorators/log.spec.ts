0c73d888c8e430b99af360ccd787d8725aab0869
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-16 18:38:39 -0300

    refactor: create makeControllerStub and makeSut for move the sut and yours stubs

--- src/decorators/log.spec.ts
+++ src/decorators/log.spec.ts
@@ -6,28 +6,48 @@ import type {
 import { LogControllerDecorator } from "./log";
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    public handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          id: "any_id",
+          email: "any_mail@mail.com",
+          name: "any_name",
+          password: "123",
+          passwordConfirmation: "123",
+        },
+      };
+
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub();
+
+  const sut = new LogControllerDecorator(controllerStub);
+
+  return {
+    sut,
+    controllerStub,
+  };
+};
+
 describe("LogController Decorator", () => {
   test("should call controller handle", async () => {
-    class ControllerStub implements Controller {
-      public handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            id: "any_id",
-            email: "any_mail@mail.com",
-            name: "any_name",
-            password: "123",
-            passwordConfirmation: "123",
-          },
-        };
-
-        return new Promise((resolve) => resolve(httpResponse));
-      }
-    }
-
-    const controllerStub = new ControllerStub();
+    const { controllerStub, sut } = makeSut();
     const controllerStubSpy = jest.spyOn(controllerStub, "handle");
-    const sut = new LogControllerDecorator(controllerStub);
 
     const httpRequest: HttpRequest = {
       body: {
