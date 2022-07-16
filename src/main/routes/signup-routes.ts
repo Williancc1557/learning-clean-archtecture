f1212673aa809129864afaaf5f567e5abd27b97e
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-15 23:29:34 -0300

    refactor: remove desnecessary console.log on signup-router

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,8 +1,6 @@
 import type { Router } from "express";
 
 export default (router: Router): void => {
-  console.log("teste");
-
   router.post("/signup", (req, res) => {
     res.json({ ok: "ok" });
   });
