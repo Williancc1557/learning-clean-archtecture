c43f7a9b9a73ad5b3eb8c684ae1dc64742be0f5f
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-07-16 12:35:38 -0300

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,17 @@
+import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
+import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
+import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+
+export const makeSignUpCOntroller = (): SignUpController => {
+  const emailVaidatorAdapter = new EmailValidatorAdapter();
+
+  const salt = 12;
+  const bcryptAdapter = new BcryptAdapter(salt);
+
+  const accountMongoRepository = new AccountMongoRepository();
+  const addAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+
+  return new SignUpController(emailVaidatorAdapter, addAccount);
+};
