f0166025b6c3571ed5dca76018b1789078692e00
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-24 12:06:35 -0300

    test: ensure LoginController returns 400 if invalid email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from "../../errors";
+import { InvalidParamError, MissingParamError } from "../../errors";
 import { badRequest } from "../../helpers/http-helper";
 import type { EmailValidator } from "../signup/signup-protocols";
 import { LoginController } from "./login";
@@ -58,7 +58,7 @@ describe("Login Controller", () => {
         expect(httpResponse).toStrictEqual(badRequest(new MissingParamError("password")));
     });
 
-    test("should call EmailValidator with correct email", async () => {
+    test("should return 400 if an invalid email is provided", async () => {
         const { sut, emailValidatorStub } = makeSut();
 
         const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
@@ -73,5 +73,22 @@ describe("Login Controller", () => {
         await sut.handle(httpRequest);
 
         expect(isValidSpy).toHaveBeenCalledWith("any_email");
+    });
+
+    test("should return 400 if EmailValidator.isValid returns false", async () => {
+        const { sut, emailValidatorStub } = makeSut();
+
+        jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false);
+
+        const httpRequest = {
+            body: {
+                email: "any_email",
+                password: "any_password",
+            },
+        };
+
+        const httpResponse = await sut.handle(httpRequest);
+
+        expect(httpResponse).toStrictEqual(badRequest(new InvalidParamError("email")));
     });
 });
