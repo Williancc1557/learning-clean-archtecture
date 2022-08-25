a7733fa9277194a6ab3028f3048bced5560344b1
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-25 13:47:50 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,26 +1,28 @@
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest } from "../../helpers/http-helper";
+import { badRequest, serverError } from "../../helpers/http-helper";
 import type { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import type { EmailValidator } from "../signup/signup-protocols";
 
 export class LoginController implements Controller {
-    public constructor(
-        private readonly emailValidator: EmailValidator
-    ) {}
+  public constructor(private readonly emailValidator: EmailValidator) {}
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { email, password } = httpRequest.body;
-        if (!email) {
-            return badRequest(new MissingParamError("email"));
-        }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { email, password } = httpRequest.body;
+      if (!email) {
+        return badRequest(new MissingParamError("email"));
+      }
 
-        if (!password) {
-            return badRequest(new MissingParamError("password"));
-        }
+      if (!password) {
+        return badRequest(new MissingParamError("password"));
+      }
 
-        if (!this.emailValidator.isValid(httpRequest.body.email)) {
-            return badRequest(new InvalidParamError("email"));
-        }
+      if (!this.emailValidator.isValid(httpRequest.body.email)) {
+        return badRequest(new InvalidParamError("email"));
+      }
+    } catch (err) {
+      return serverError();
     }
+  }
 }
