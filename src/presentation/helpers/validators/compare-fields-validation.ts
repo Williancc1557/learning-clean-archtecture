3b44f0181ea3ad5874a776bec816ec9b55f030d7
Willian Cavalcanti <willian.cacoelho@gmail.com>
2022-08-28 01:10:11 -0300

    fix: ensure CompareFieldsValidation get the passwordConfirmation field

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -9,7 +9,7 @@ export class CompareFieldsValidation implements Validation {
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public validate(input: any): Error {
-    if (input[this.fieldName] != this.fieldToCompareName) {
+    if (input[this.fieldName] != input[this.fieldToCompareName]) {
       return new InvalidParamError(this.fieldToCompareName);
     }
   }
